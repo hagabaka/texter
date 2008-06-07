6760dbf209a682ec8acae23ee2045c587377e970
Yaohan Chen <yaohan.chen@gmail.com>
2008-06-07 11:41:46 -0400

    add spec for parser

---
+++ spec/parser.rb
@@ -0,0 +1,62 @@
+require 'test/spec'
+require 'texter/parser'
+require 'set'
+
+context 'RubyCodeParser' do
+  class MockHandler
+    attr_accessor :handled_strings
+
+    def initialize
+      @handled_strings = []
+    end
+
+    def string(n)
+      @handled_strings << n
+    end
+
+    def ignore(n)
+    end
+  end
+
+  class String
+    def ruby_strings
+      handler = MockHandler.new
+      parser = RubyCodeParser.new
+      parser.parse(self).process(handler)
+      handler.handled_strings
+    end
+  end
+
+  specify 'The parser should be able to extract strings' do
+    code =<<-EOS
+      aaa 'bbb' ccc("ddd", eee)
+    EOS
+    code.ruby_strings.map {|n|
+      n.text_value
+    }.to_set.should.equal %w['bbb' "ddd"].to_set
+  end
+
+  specify 'The parser should handle escaped quotes correctly' do
+    code =<<-EOS
+      "foo\"bar\"" 'foo\'bar'
+    EOS
+    code.ruby_strings.map {|n|
+      n.text_value
+    }.to_set.should.equal %w["foo"bar", 'foo'bar'].to_set
+  end
+
+  specify 'The parser should ignore quotes in comments' do
+    code =<<-EOS
+      # comment containing " and ' "
+    EOS
+
+    code.ruby_strings.should.be.empty
+  end
+
+  specify 'The parser should ignore quotes in regexp and % literals' do
+    code =<<-EOS
+      /'a"/ %r(a 'a') %Q{"foo"}
+    EOS
+    code.ruby_strings.should.be.empty
+  end
+end
