47d4884472249ead37d4a6ddcc3c4efac0ec415f
Yaohan Chen <yaohan.chen@gmail.com>
2008-06-07 12:52:46 -0400

    move information text into README

--- texter.rb
+++ texter.rb
@@ -1,35 +1,4 @@
 #!/usr/bin/ruby
-#
-# texter is a utility to help you modify ruby source programs in order to use gettext.
-# It searches for string literals, display each and ask you whether it should be
-# marked as translatable, with _( ). In case the string contains embedded code, it
-# is able to rewrite it with string modulo operator with your input
-#
-# USAGE
-#   texter.rb <input-file> [<output-file>]
-# output-file is by default <named input-file>.texter.rb
-#
-# AUTHOR
-# Yaohan Chen <yaohan.chen@gmail.com>
-#
-# LICENSE
-# Ruby's
-#
-# CREDITS
-# - Daniel Brumbaugh Keeney provided much help with the treetop grammar and rest of
-#   the program
-#
-# BUGS
-# - The grammar treats ' and " inside regexp, here-doc, %( ) etc as string delimiters
-# - Only strings enclosed in ' and " are handled
-#
-# TODO
-# - Default answers for yes/no prompts
-# - Preview
-# (following probably too difficult)
-# - Save and restore progress
-# - Recover from incorrect parsing
-# - Undo
 
 require 'texter/highline-handler.rb'
 require 'texter/parser.rb'
